40890a4cb8bf542a0108876703b8c070c1e51d94
TOM_RUS <tomrus88@github.com>
2014-04-21 00:39:12 +0400

    Fix

--- CASCExplorer/ExtractProgress.cs
+++ CASCExplorer/ExtractProgress.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows.Forms;
-using System.ComponentModel;
 
 namespace CASCExplorer
 {
