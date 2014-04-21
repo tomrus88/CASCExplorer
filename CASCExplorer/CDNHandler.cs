da97151be21edaa3d4d5f3574e5abae1341e6c65
TOM_RUS <tomrus88@github.com>
2014-04-21 20:16:03 +0400

    Fix

--- CASCExplorer/CDNHandler.cs
+++ CASCExplorer/CDNHandler.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CASCExplorer
 {
@@ -12,14 +8,28 @@ namespace CASCExplorer
     {
         static Dictionary<string, MemoryStream> Indexes = new Dictionary<string, MemoryStream>();
 
-        public static void Initialize()
+        public static void Initialize(bool local)
         {
-            WebClient client = new WebClient();
-
-            foreach (var index in CASCConfig.CDNConfig["archives"])
+            if (local)
             {
-                var url = CASCConfig.CDNUrl + "/data/" + index.Substring(0, 2) + "/" + index.Substring(2, 2) + "/" + index + ".index";
-                Indexes.Add(index, new MemoryStream(client.DownloadData(url)));
+                string wowPath = Properties.Settings.Default.WowPath;
+
+                foreach (var index in CASCConfig.CDNConfig["archives"])
+                {
+                    var path = Path.Combine(wowPath, "Data\\indices\\", index + ".index");
+                    Indexes.Add(index, new MemoryStream(File.ReadAllBytes(path)));
+                }
+            }
+            else
+            {
+                using (WebClient client = new WebClient())
+                {
+                    foreach (var index in CASCConfig.CDNConfig["archives"])
+                    {
+                        var url = CASCConfig.CDNUrl + "/data/" + index.Substring(0, 2) + "/" + index.Substring(2, 2) + "/" + index + ".index";
+                        Indexes.Add(index, new MemoryStream(client.DownloadData(url)));
+                    }
+                }
             }
         }
     }
