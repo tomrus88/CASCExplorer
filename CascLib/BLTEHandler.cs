75efacd73a42159be4159005c0e0438d6a49a59f
TOM_RUS <tomrus88@github.com>
2015-10-30 00:19:00 +0300

    Fix

--- CascLib/BLTEHandler.cs
+++ CascLib/BLTEHandler.cs
@@ -140,8 +140,17 @@ namespace CASCExplorer
 
         static Dictionary<ulong, byte[]> keys = new Dictionary<ulong, byte[]>()
         {
-            [0X402CD9D8D6BFED98] = new byte[] { 0xAE, 0xB0, 0xEA, 0xDE, 0xA4, 0x76, 0x12, 0xFE, 0x6C, 0x04, 0x1A, 0x03, 0x95, 0x8D, 0xF2, 0x41 },
-            [0xFB680CB6A8BF81F3] = new byte[] { 0x62, 0xD9, 0x0E, 0xFA, 0x7F, 0x36, 0xD7, 0x1C, 0x39, 0x8A, 0xE2, 0xF1, 0xFE, 0x37, 0xBD, 0xB9 },
+            // hardcoded keys
+            [0x402CD9D8D6BFED98] = "AEB0EADEA47612FE6C041A03958DF241".ToByteArray(),
+            [0xFB680CB6A8BF81F3] = "62D90EFA7F36D71C398AE2F1FE37BDB9".ToByteArray(),
+            // streamed keys
+            [0xDBD3371554F60306] = "34E397ACE6DD30EEFDC98A2AB093CD3C".ToByteArray(),
+            [0x11A9203C9881710A] = "2E2CB8C397C2F24ED0B5E452F18DC267".ToByteArray(),
+            [0xA19C4F859F6EFA54] = "0196CB6F5ECBAD7CB5283891B9712B4B".ToByteArray(),
+            [0x87AEBBC9C4E6B601] = "685E86C6063DFDA6C9E85298076B3D42".ToByteArray(),
+            [0xDEE3A0521EFF6F03] = "AD740CE3FFFF9231468126985708E1B9".ToByteArray(),
+            [0x8C9106108AA84F07] = "53D859DDA2635A38DC32E72B11B32F29".ToByteArray(),
+            //[0x49166D358A34D815] = "00000000000000000000000000000000".ToByteArray(),
         };
 
         private static void Decrypt(byte[] data, Stream outS)
