705ff3ddf77047d55296e7a2f210e45ff76bfcee
TOM_RUS <tomrus88@github.com>
2015-10-31 01:18:26 +0300

    Fix

--- CascLib/BLTEHandler.cs
+++ CascLib/BLTEHandler.cs
@@ -138,21 +138,6 @@ namespace CASCExplorer
             }
         }
 
-        static Dictionary<ulong, byte[]> keys = new Dictionary<ulong, byte[]>()
-        {
-            // hardcoded keys
-            [0x402CD9D8D6BFED98] = "AEB0EADEA47612FE6C041A03958DF241".ToByteArray(),
-            [0xFB680CB6A8BF81F3] = "62D90EFA7F36D71C398AE2F1FE37BDB9".ToByteArray(),
-            // streamed keys
-            [0xDBD3371554F60306] = "34E397ACE6DD30EEFDC98A2AB093CD3C".ToByteArray(),
-            [0x11A9203C9881710A] = "2E2CB8C397C2F24ED0B5E452F18DC267".ToByteArray(),
-            [0xA19C4F859F6EFA54] = "0196CB6F5ECBAD7CB5283891B9712B4B".ToByteArray(),
-            [0x87AEBBC9C4E6B601] = "685E86C6063DFDA6C9E85298076B3D42".ToByteArray(),
-            [0xDEE3A0521EFF6F03] = "AD740CE3FFFF9231468126985708E1B9".ToByteArray(),
-            [0x8C9106108AA84F07] = "53D859DDA2635A38DC32E72B11B32F29".ToByteArray(),
-            //[0x49166D358A34D815] = "00000000000000000000000000000000".ToByteArray(),
-        };
-
         private static void Decrypt(byte[] data, Stream outS)
         {
             byte keyNameSize = data[1];
@@ -166,8 +151,8 @@ namespace CASCExplorer
 
             byte IVSize = data[keyNameSize + 2];
 
-            if (IVSize > 0x10)
-                throw new Exception("IVSize > 0x10");
+            if (IVSize != 4 || IVSize > 0x10)
+                throw new Exception("IVSize != 4 || IVSize > 0x10");
 
             byte[] IVpart = data.Skip(keyNameSize + 3).Take(IVSize).ToArray();
 
@@ -196,9 +181,9 @@ namespace CASCExplorer
             //    IV[j] ^= (byte)(someValue >> i);
             //}
 
-            byte[] key;
+            byte[] key = KeyService.GetKey(keyName);
 
-            if (!keys.TryGetValue(keyName, out key))
+            if (key == null)
             {
                 string msg = string.Format("Unknown keyname {0:X16}", keyName);
                 //Logger.WriteLine(msg);
---
+++ CascLib/KeyService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CASCExplorer
+{
+    class KeyService
+    {
+        static Dictionary<ulong, byte[]> keys = new Dictionary<ulong, byte[]>()
+        {
+            // hardcoded Overwatch keys
+            [0x402CD9D8D6BFED98] = "AEB0EADEA47612FE6C041A03958DF241".ToByteArray(),
+            [0xFB680CB6A8BF81F3] = "62D90EFA7F36D71C398AE2F1FE37BDB9".ToByteArray(),
+            // streamed Overwatch keys
+            [0xDBD3371554F60306] = "34E397ACE6DD30EEFDC98A2AB093CD3C".ToByteArray(),
+            [0x11A9203C9881710A] = "2E2CB8C397C2F24ED0B5E452F18DC267".ToByteArray(),
+            [0xA19C4F859F6EFA54] = "0196CB6F5ECBAD7CB5283891B9712B4B".ToByteArray(),
+            [0x87AEBBC9C4E6B601] = "685E86C6063DFDA6C9E85298076B3D42".ToByteArray(),
+            [0xDEE3A0521EFF6F03] = "AD740CE3FFFF9231468126985708E1B9".ToByteArray(),
+            [0x8C9106108AA84F07] = "53D859DDA2635A38DC32E72B11B32F29".ToByteArray(),
+            //[0x49166D358A34D815] = "00000000000000000000000000000000".ToByteArray(),
+        };
+
+        public static byte[] GetKey(ulong keyName)
+        {
+            byte[] key;
+            keys.TryGetValue(keyName, out key);
+            return key;
+        }
+    }
+}
