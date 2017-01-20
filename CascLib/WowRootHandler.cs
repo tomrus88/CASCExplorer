16345344408fca3729381887b7ad08eea89bea59
TOM_RUS <tomrus88@github.com>
2017-01-20 05:09:17 +0300

    Fix

--- CascLib/WowRootHandler.cs
+++ CascLib/WowRootHandler.cs
@@ -39,6 +39,8 @@ namespace CASCExplorer
         F00000001 = 0x1,
         F00000002 = 0x2,
         F00000004 = 0x4,
+        F00000008 = 0x8, // added in 7.2.0.23436
+        F00000010 = 0x10, // added in 7.2.0.23436
         LowViolence = 0x80, // many models have this flag
         F10000000 = 0x10000000,
         F20000000 = 0x20000000, // added in 21737
@@ -83,7 +85,7 @@ namespace CASCExplorer
                 if (localeFlags == LocaleFlags.None)
                     throw new Exception("block.LocaleFlags == LocaleFlags.None");
 
-                if (contentFlags != ContentFlags.None && (contentFlags & (ContentFlags.LowViolence | ContentFlags.NoCompression | ContentFlags.F20000000)) == 0)
+                if (contentFlags != ContentFlags.None && (contentFlags & (ContentFlags.F00000008 | ContentFlags.F00000010 | ContentFlags.LowViolence | ContentFlags.NoCompression | ContentFlags.F20000000)) == 0)
                     throw new Exception("block.ContentFlags != ContentFlags.None");
 
                 RootEntry[] entries = new RootEntry[count];
